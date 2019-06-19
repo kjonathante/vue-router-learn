209ec9ca3820ccf1d7c1ed405f48592c507c4018
Kit Jonathan Te <kjonathante@gmail.com>
2019-06-18 21:20:25 -0700

    Modified code and added some code snippets from https://router.vuejs.org/guide/

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+      <!-- <router-link to="/foo">Go to Foo</router-link> -->
+      <!-- <router-link to="/bar">Go to Bar</router-link> -->
     </div>
-    <router-view />
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view/>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="goBack">Back</button>
   </div>
 </template>
+<script>
+// import Name from './components/Name.vue'
+
+export default {
+  computed: {
+    username() {
+      // We will see what `params` is shortly
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
+  }
+}
+</script>
